e4988348bf6f5f93245f6fa428e650ea01a76a16
Akanksha <akankshamdr@gmail.com>
2022-09-23 19:18:47 +0530

    initialisation

---
+++ script.js
@@ -0,0 +1,16 @@
+// document.querySelector('.cross').style.display='none'
+// document.querySelector('.hamburger').addEventListener('click',()=>{
+//     document.querySelector('.sidebar').classList.toggle('.sidebargo') 
+//     console.log("hello")
+// if(document.querySelector('.sidebar').classList.contains('.sidebargo')){
+//     document.querySelector('.ham').style.display='inline'
+//     document.querySelector('.cross').style.display='none'
+// }
+// else{
+//     document.querySelector('.cross').style.display='inline'
+//     setTimeout(()=>{
+//         document.querySelector('.ham').style.display='none'
+
+//     } ,)
+// }
+// })
